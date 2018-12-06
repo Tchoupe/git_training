03434626326a42b82eb94d16d4187a1c7f79bc6c
event event <event@w-r10-p3.s19.be>
2018-12-06 13:49:32 +0100

    Remove fun from tom

--- bouffe.py
+++ bouffe.py
@@ -1,31 +1,28 @@
 import time
 import os
 
+os.system('clear')
 while 1:
     print(" (.Y.)")
     print(" ) . (")
     print(" ( Y )")
-    print()
     time.sleep(0.1)
     os.system('clear')
-
+    
     print(" (.Y.)")
     print("  ) . (")
     print(" ( Y )")
-    print()
     time.sleep(0.1)
     os.system('clear')
 
     print(" (.Y.)")
     print(" ) . (")
     print(" ( Y )")
-    print()
     time.sleep(0.1)
     os.system('clear')
 
     print(" (.Y.)")
     print(") . (")
     print(" ( Y )")
-    print()
     time.sleep(0.1)
     os.system('clear')
