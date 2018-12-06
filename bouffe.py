09a48ab77cb56847b145168e0daa72e757e6f62a
event event <event@w-r10-p5.s19.be>
2018-12-06 13:28:09 +0100

    supplement fromage

---
+++ bouffe.py
@@ -0,0 +1,31 @@
+import time
+import os
+
+while 1:
+    print(" (.Y.)")
+    print(" ) . (")
+    print(" ( Y )")
+    print()
+    time.sleep(0.1)
+    os.system('cls')
+
+    print(" (.Y.)")
+    print("  ) . (")
+    print(" ( Y )")
+    print()
+    time.sleep(0.1)
+    os.system('cls')
+
+    print(" (.Y.)")
+    print(" ) . (")
+    print(" ( Y )")
+    print()
+    time.sleep(0.1)
+    os.system('cls')
+
+    print(" (.Y.)")
+    print(") . (")
+    print(" ( Y )")
+    print()
+    time.sleep(0.1)
+    os.system('cls')
