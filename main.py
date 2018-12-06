1dca34940618e0fd153c0888c73282204fc38311
event event <event@w-r10-p3.s19.be>
2018-12-06 13:03:10 +0100

    Update spanish

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import time
 
 if __name__ == "__main__":
-    for i in range(50):
+    for i in range(10):
         print('hola como estas')
         time.sleep(0.5)
     print('esta bien, y tu ?')
+
